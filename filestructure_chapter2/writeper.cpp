f8f1e620768ade56d09383c2f7c10014778fea67
brown0394 <73463402+brown0394@users.noreply.github.com>
2022-03-10 14:42:23 +0900

    debugging writeper
    debug completed
    should work for wirting readper.cpp

--- filestructure_chapter2/writeper.cpp
+++ filestructure_chapter2/writeper.cpp
@@ -1,11 +1,13 @@
 // D.7 writeper.cpp
 // write a stream of persons, using fstream.h 
 //Assignment to students - writestr.cpp를 참조하여 수정하여 완성: 실습
-#include <fstream.h>
+#include <fstream>
+#include <iostream>
 #include <string.h>
-#include "readper.cpp"
+//#include "readper.cpp"구현해볼것.
+#include "person.h"
 const int MaxBufferSize = 200;
-int WritePerson (ostream & stream, Person & p)
+int WritePerson (std::ostream & stream, Person & p)
 {	char buffer [MaxBufferSize];
 	strcpy(buffer, p.LastName); strcat(buffer,"|");
 	strcat(buffer, p.FirstName); strcat(buffer,"|");
@@ -15,14 +17,15 @@ int WritePerson (ostream & stream, Person & p)
 	strcat(buffer, p.ZipCode);  strcat(buffer,"|");
 	short length=strlen(buffer); 
 	stream.write ((char *)&length, sizeof(length)); // write length
-	stream.write (&buffer, length);
+	stream.write (buffer, length);
+	return 1;
 }
 
 int main (){
 	char filename [20];
 	Person p;
-	cout << "Enter the file name:"<<flush;
-	cin.getline(filename, 19);
+	std::cout << "Enter the file name:"<<flush;
+	std::cin.getline(filename, 19);
 	ofstream stream (filename, ios::out);
 	if (stream.fail()) {
 		cout << "File open failed!" <<endl;
